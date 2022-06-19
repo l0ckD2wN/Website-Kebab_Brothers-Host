afc800953f07c7e3163f7a9095134416977002d9
l0ckD2wN <Gunmetaltiger35@gmx.de>
2022-06-19 16:31:28 +0200

    added everything to host the website (for testing)

---
+++ code/main.js
@@ -0,0 +1,21 @@
+/*const mobileOpenMenu = document.getElementById('mobile-menu-open');
+const nav = document.getElementById('navbar')
+const mobileCloseMenu = document.getElementById('mobile-menu-close');
+
+mobileOpenMenu.addEventListener('click', () => {
+    nav.classList.add('navbar');
+})
+
+mobileCloseMenu.addEventListener('click', () => {
+    nav.classList.remove('navbar');
+})*/
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/*function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}*/
